d626bc3d71b0ef8d5d913c0a2f659e39d01f18ab
Decaf Code <git@decafcode.org>
2017-04-07 10:48:37 -0400

    SimpleExceptionFilter: Catch uncaught exceptions and log to servlet log

--- crest-exception/src/main/java/org/decafcode/crest/exception/SimpleExceptionFilter.java
+++ crest-exception/src/main/java/org/decafcode/crest/exception/SimpleExceptionFilter.java
@@ -5,6 +5,7 @@ import javax.inject.Singleton;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -12,8 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 
 @Singleton
 public class SimpleExceptionFilter implements Filter {
+    private ServletContext ctx;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        ctx = filterConfig.getServletContext();
     }
 
     @Override
@@ -21,17 +25,23 @@ public class SimpleExceptionFilter implements Filter {
             ServletRequest req_,
             ServletResponse resp_,
             FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse resp = (HttpServletResponse) resp_;
+
         try {
             chain.doFilter(req_, resp_);
         } catch (HttpStatusException e) {
-            HttpServletResponse resp = (HttpServletResponse) resp_;
-
             e.extraHeaders().entrySet().forEach(kvp -> resp.setHeader(
                     kvp.getKey(),
                     kvp.getValue()));
             resp.setStatus(e.statusCode());
             resp.setContentType("text/plain;charset=utf-8");
             resp.getWriter().print(e.getMessage());
+        } catch (Throwable t) {
+            ctx.log("Uncaught exception", t);
+
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.setContentType("text/plain;charset=utf-8");
+            resp.getWriter().print("Internal Server Error");
         }
     }
 

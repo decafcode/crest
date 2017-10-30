36374db3d2e83d7151d6e1ecb388feeda10e2017
Decaf Code <git@decafcode.org>
2017-10-30 14:50:42 -0400

    HttpStatusException: Require non-null message

--- crest-exception/src/main/java/org/decafcode/crest/exception/HttpStatusException.java
+++ crest-exception/src/main/java/org/decafcode/crest/exception/HttpStatusException.java
@@ -2,15 +2,16 @@ package org.decafcode.crest.exception;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 import javax.servlet.ServletException;
 
 public abstract class HttpStatusException extends ServletException {
     public HttpStatusException(String message) {
-        super(message);
+        super(Objects.requireNonNull(message, "message"));
     }
 
     public HttpStatusException(String message, Throwable cause) {
-        super(message, cause);
+        super(Objects.requireNonNull(message, "message"), cause);
     }
 
     public Map<String, String> extraHeaders() {
